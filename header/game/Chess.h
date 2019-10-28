cef6fce3231274af395c7ee5ad89b2517a2d07db
Blue Eu <euisblue@pm.me>
2019-10-28 09:03:33 -0700

    styling modified

--- header/game/Chess.h
+++ header/game/Chess.h
@@ -1,7 +1,7 @@
 /*
-	Author: Jii Myoi
-	Date: 21 April 2016
-	Specification file for the Chess class
+Author: Jii Yoo
+Date: 21 April 2016
+Specification file for the Chess class
 */
 
 #ifndef CHESS_H
@@ -14,35 +14,35 @@
 using namespace Color;
 
 class Chess {
-	// Let PieceMove class access Chess' private variables
-	friend class PieceMove;
+    // Let PieceMove class access Chess' private variables
+    friend class PieceMove;
 
-	private:
-		User *usr;
-		Stack stck_mv;	
+    private:
+        User *usr;
+        Stack stck_mv;	
 
-		char **cdp_board;  // chess board
-		char crg_from[2];  // move piece location from->to
-		char crg_to[2];
-		
-		// Helper functions;
-		void chessInit( void ); // game - body/logic
-		void mkMove( bool );
-		bool chckMove( bool );
-		bool isValid( string & );
-		void gameover( void );
-		void playGame( void );
+        char **cdp_board;  // chess board
+        char crg_from[2];  // move piece location from->to
+        char crg_to[2];
 
-		void display( void ); // game - graphic(?) part
-		void drawPcs( void );
-		void cls( void );
-		void status( void );
+        // Helper functions;
+        void chessInit( void ); // game - body/logic
+        void mkMove( bool );
+        bool chckMove( bool );
+        bool isValid( string & );
+        void gameover( void );
+        void playGame( void );
 
-		void scrBoard( bool );  // file IO
-	public:
-		Chess() { chessInit(); } // Constructor
-		~Chess( void ); // Destructor
-		void menu( void );
+        void display( void ); // game - graphic(?) part
+        void drawPcs( void );
+        void cls( void );
+        void status( void );
+
+        void scrBoard( bool );  // file IO
+    public:
+        Chess() { chessInit(); } // Constructor
+        ~Chess( void ); // Destructor
+        void menu( void );
 };
 
 #endif
