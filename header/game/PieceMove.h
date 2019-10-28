2d731c4700fbca51a49ad995d7662d0471972d86
Blue Eu <euisblue@pm.me>
2019-10-28 09:03:33 -0700

    styling modified

--- header/game/PieceMove.h
+++ header/game/PieceMove.h
@@ -1,7 +1,7 @@
 /*
-	Author: Jii Myoi
-	Date: April 29, 2016
-  	Secification file for the PieceMove class 
+Author: Jii Yoo
+Date: April 29, 2016
+Secification file for the PieceMove class 
 */
 
 #ifndef PIECEMOVE_H
@@ -9,26 +9,26 @@
 #include "../game/Chess.h"
 
 class PieceMove{
-	private:
-		char ch_piece;
-		short sh_promote;
-		
-		// helper functions
-		bool fn_kingMv(Chess *);
-		bool fn_queenMv(Chess *);
-		bool fn_bshpMv(Chess *);
-		bool fn_knghtMv(Chess *); 
-		bool fn_rookMv(Chess *);
-		bool fn_pawnMv(Chess *);
-		
-		void fn_kill(Chess *, char);
-	public:
-		// Accessor
-		short fn_getPrmt()
-			{ return sh_promote; }
+    private:
+        char ch_piece;
+        short sh_promote;
 
-		// Member functions
-		bool fn_validMv(Chess *); // check for valid move
+        // helper functions
+        bool fn_kingMv(Chess *);
+        bool fn_queenMv(Chess *);
+        bool fn_bshpMv(Chess *);
+        bool fn_knghtMv(Chess *); 
+        bool fn_rookMv(Chess *);
+        bool fn_pawnMv(Chess *);
+
+        void fn_kill(Chess *, char);
+    public:
+        // Accessor
+        short fn_getPrmt()
+        { return sh_promote; }
+
+        // Member functions
+        bool fn_validMv(Chess *); // check for valid move
 };
 
 #endif
