03f3a684ddef94a327a8d95ecf9249b9474f1ddf
Madelen Yoo <rolemadelen@tuta.io>
2019-10-28 09:04:35 -0700

    styling modified
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- header/stack/Node.h
+++ header/stack/Node.h
@@ -1,16 +1,16 @@
 /* 
-	Author: Jii Myoi
-	Date: April 29, 2016
-	Specification file for the Node structure 
+Author: Jii Yoo
+Date: April 29, 2016
+Specification file for the Node structure 
 */
 
 #ifndef NODE_H
 #define NODE_H
 
 struct Node {
-	Node *ptr;
-	char from[2]; // chess moves [from->to]
-	char to[2];
+    Node *ptr;
+    char from[2]; // chess moves [from->to]
+    char to[2];
 };
 
 #endif
