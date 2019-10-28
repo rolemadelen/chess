d1b3bbf8e654c91eded99ff5f555132ab27b5a11
rolemadelen <rolemadelen17@pm.me>
2019-10-28 09:36:50 -0700

    styling modified

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,6 @@
 /*
-	Author: Jii Myoi
-	Date: 21 April, 2016
-	Run the CHESS!!
+Author: Jii Yoo
+Date: 21 April, 2016
 */
 
 // System Libraries
@@ -12,8 +11,9 @@ using namespace std;
 #include "../header/game/Chess.h"
 
 // Execution begins here
-int main(int argc, char **argv) {
-	Chess game;
-	game.menu();
-	return 0;
+int main(int argc, char **argv) 
+{
+    Chess game;
+    game.menu();
+    return 0;
 }
