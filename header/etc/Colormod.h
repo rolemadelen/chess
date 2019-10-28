87d65fd5dd860d59b998df1326d50c835fd43599
Madelen Yoo <rolemadelen@tuta.io>
2019-10-28 09:02:31 -0700

    styling modified
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- header/etc/Colormod.h
+++ header/etc/Colormod.h
@@ -1,8 +1,8 @@
 /*
-    Author: This piece of code does not belong to myself.	
+Author: This piece of code does not belong to myself.	
 
-    Purpose:  change text color in console.
-          usage of "\033[": handle the console cursor            
+Purpose:  change text color in console.
+usage of "\033[": handle the console cursor            
 */
 
 #ifndef COLORMOD_H
@@ -10,25 +10,24 @@
 
 #include <ostream>
 namespace Color {
-	enum Code {
-		FG_RED     = 31,
-		FG_GREEN   = 32,
-		FG_BLUE    = 34,
-		FG_DEFAULT = 39,
-		BG_RED     = 41,
-		BG_GREEN   = 42,
-		BG_BLUE    = 44,
-		BG_DEFAULT = 49
-	};
-	class Modifier {
-	Code code;
-	public:
-		Modifier(Code pCode):code(pCode){}
-		friend std::ostream&
-		operator<<(std::ostream& os, const Modifier& mod) {
-			return os << "\033["<<mod.code<<"m";
-		}
-	}; // end class
+    enum Code {
+        FG_RED     = 31,
+        FG_GREEN   = 32,
+        FG_BLUE    = 34,
+        FG_DEFAULT = 39,
+        BG_RED     = 41,
+        BG_GREEN   = 42,
+        BG_BLUE    = 44,
+        BG_DEFAULT = 49
+    };
+    class Modifier {
+        Code code;
+        public:
+        Modifier(Code pCode):code(pCode){}
+        friend std::ostream& operator<< (std::ostream& os, const Modifier& mod) {
+            return os << "\033["<<mod.code<<"m";
+        }
+    }; // end class
 } // end namespace
 
 #endif
--- header/etc/PreciseClock.h
+++ header/etc/PreciseClock.h
@@ -1,23 +1,23 @@
 /*
-Copyright (c) 2010-2013 Tommaso Urli
-Tommaso Urli    tommaso.urli@uniud.it   University of Udine
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+   Copyright (c) 2010-2013 Tommaso Urli
+   Tommaso Urli    tommaso.urli@uniud.it   University of Udine
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+   LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   */
 
 #ifndef PRECISECLOCK_H
 #define PRECISECLOCK_H
@@ -39,35 +39,35 @@ typedef unsigned long long uint64;
 
 uint64 GetTimeMs64() {
 #ifdef _WIN32
-	/* Windows */
-	FILETIME ft;
-	LARGE_INTEGER li;
+    /* Windows */
+    FILETIME ft;
+    LARGE_INTEGER li;
 
-	/* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
-	 * to a LARGE_INTEGER structure. */
-	GetSystemTimeAsFileTime(&ft);
-	li.LowPart = ft.dwLowDateTime;
-	li.HighPart = ft.dwHighDateTime;
+    /* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
+     * to a LARGE_INTEGER structure. */
+    GetSystemTimeAsFileTime(&ft);
+    li.LowPart = ft.dwLowDateTime;
+    li.HighPart = ft.dwHighDateTime;
 
-	uint64 ret = li.QuadPart;
-	ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
-	ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
+    uint64 ret = li.QuadPart;
+    ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+    ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
 
-	return ret;
+    return ret;
 #else
-	/* Linux */
-	struct timeval tv;
+    /* Linux */
+    struct timeval tv;
 
-	gettimeofday(&tv, NULL);
+    gettimeofday(&tv, NULL);
 
-	uint64 ret = tv.tv_usec;
-	/* Convert from micro seconds (10^-6) to milliseconds (10^-3) */
-	ret /= 1000;
+    uint64 ret = tv.tv_usec;
+    /* Convert from micro seconds (10^-6) to milliseconds (10^-3) */
+    ret /= 1000;
 
-	/* Adds the seconds (10^0) after converting them to milliseconds (10^-3) */
-	ret += (tv.tv_sec * 1000);
+    /* Adds the seconds (10^0) after converting them to milliseconds (10^-3) */
+    ret += (tv.tv_sec * 1000);
 
-	return ret;
+    return ret;
 #endif
 }
 
